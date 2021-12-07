c5454a0ee3f05c29e33d4436b834445517163e03
iago <iago@noreply.codeberg.org>
2021-12-07 21:44:09 +0100

    Day 6

---
+++ day6/solution2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+data = open("input").read()
+fishes = data.split(",")
+
+days = 256   # Amount of days to simulate
+
+for day in range(0, days):
+    for fish in range(0, len(fishes)):
+        fish = int(fish)
+        if int(fishes[fish]) == 0:
+            fishes[fish] = 6
+            fishes.append(8)
+        else:
+            fishes[fish] = int(fishes[fish]) - 1
+
+total = len(fishes)
+print(total)
