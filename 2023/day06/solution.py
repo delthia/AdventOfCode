4687b3bb9cc59037788d78b8b5a8f52832ba082e
delthia <git@delthia.com>
2023-12-06 10:35:12 +0100

    Day 6 - part 2

--- 2023/day06/solution.py
+++ 2023/day06/solution.py
@@ -22,12 +22,46 @@ def possible_records(races:list[tuple]) -> list:
 
     return records
 
+def multiply_records(records:list) -> int:
+    total = 1
+    for record in records:
+        total *= record
+
+    return total
+
+def record_range(time:int, record:int) -> list:
+    winning = []
+    for speed in range(time):
+        distance = speed * (time - speed)
+        if distance > record:
+            winning.append(speed)
+            break
+
+    for speed in range(time, 0, -1):
+        distance = speed * (time - speed)
+        if distance > record:
+            winning.append(speed)
+            break
+
+    possible_wins = winning[1] - winning[0] + 1 # Include all the possibilities
+    print(f"from {winning[0]} to {winning[1]}")
+    return possible_wins
+
 if __name__ == '__main__':
     # races = parse_input('test-input')
     races = parse_input('input')
 
+    # Part 1
     records = possible_records(races)
-    total = 1
-    for record in records:
-        total *= record
+    total = multiply_records(records)
     print(f"The total number of records multiplied toghether is {total}")
+
+    # Part 2
+    print(races)
+    time = int(''.join([str(pos[0]) for pos in races]))
+    record = int(''.join([str(pos[1]) for pos in races]))
+    print(time)
+    print(record)
+    print("--- Part 2 ---")
+    winning = record_range(time, record)
+    print(f"You can beat the record {winning} different ways")
